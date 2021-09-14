0846e868686df7aa83cd8421c61ba564be1bc2f2
CesarJimenezVilleda02 <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2021-09-14 13:48:34 -0500

    minas random

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ print("Bienvenido al buscaminas ")
 def crearTablero(filas, columnas):
     return 0
 
-def colocarminas():
-    return 0
+def colocarminas(numMinas):
+    respuestas = crearTablero(filas, columnas)
+    for i in range(0, numMinas):
+        posx = random.randint(0, columnas-1)
+        posy = random.randint(0, filas-1)
+        while respuestas[posy][posx] == "X":
+            posx = random.randint(0, columnas-1)
+            posy = random.randint(0, filas-1)
+        else:
+            respuestas[posy].insert(posx, "X")
+            respuestas[posy].pop(posx+1)
+    return respuestas
 
 def verificarAlrededor():
     return 0

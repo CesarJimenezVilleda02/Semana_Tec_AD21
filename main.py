d7d5e4cb7b5470ca813806b5a9d0cac867ba9141
AriannFernando <A01703556@itesm.mx>
2021-09-15 00:15:53 -0500

    Definición de variables para la creación del tablero

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+"Ariann Fernando Arriaga Alcántara A01703556"
+"Pablo Cesar Jiménez Villeda"
+"Proyecto Semana_TEC AD2021"
 import random
 
 print("Bienvenido al buscaminas ")
+filas = int(input("Ingrese el número de filas que desea en su tablero "))
+columnas = int(input("Ingrese el número de columnas que desea en su tablero "))
+numminas = int(input("Ingrese el número de minas que desea en su tablero "))
 
 def crearTablero(filas, columnas):
     return 0
